cb5c1105bc189dd6cfde31f500873190cff24e8e
Avichai Ben David <69721418+avihyb@users.noreply.github.com>
2024-01-28 12:46:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,38 +8,38 @@ int main(){
     scanf("%d", &a);
     scanf("%d", &b);
     
-     printf("The Armstrong numbers are: ");
+     printf("The Armstrong numbers are:");
         for(int i = a; i <= b; i++){
             if(isArmstrong(i) == 1){
-                printf("%d ", i);
+                printf(" %d", i);
             }
         }
-    printf("\b");
+    
      printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for(int i = a; i <= b; i++){
         if(isPalindrome(i) == 1){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
-    printf("\b");
+    
         printf("\n");
-        printf("The Prime numbers are: ");
+        printf("The Prime numbers are:");
     
     for(int i = a; i <= b; i++){
         if(isPrime(i) == 1){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\b");
+    
     printf("\n");
    
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for(int i = a; i <= b; i++){
         if(isStrong(i) == 1){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
-   printf("\b");
+   
     printf("\n"); 
 }
