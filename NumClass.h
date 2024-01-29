27b6378da82c136f0692633beee207a6513d05ab
Avichai Ben David <69721418+avihyb@users.noreply.github.com>
2024-01-29 15:08:18 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,7 +1,7 @@
 #ifndef NUM_CLASS_H
 #define NUM_CLASS_H
 int isArmstrong(int x);
-int isPalindrome(int);
+int isPalindrome(int x);
 int isPrime(int x);
 int isStrong(int x);
 int power(int num, int pow);
